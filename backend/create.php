1528ab89f8d22fda0f5e63d969044db91521828a
Soufiane <soufiane@angelgrafico.com>
2023-03-28 18:29:46 +0200

    ✨ create

--- backend/create.php
+++ backend/create.php
@@ -11,50 +11,56 @@
     <body id="create">
         <main class="container pb-4">
             <?php include("header.php");?>
-            <div class="createForm">
+            
+            <form action="functions.php" method="POST" id="form-create" enctype="multipart/form-data">
                 <h1 class="display-5">Nueva noticia</h1>
                 <p class="lead">Rellene la información deseada para publicar una nueva noticia.</p>
                 <hr class="my-2">
-
-                <form action="functions.php" method="POST" id="form-create" enctype="multipart/form-data">
+        
+                <div class="createForm">
                     <div class="form-group row">
                         <label for="new-title-input" class="col-sm-2 col-form-label">Título</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="new-title-input" name="title-create" placeholder="Título de la noticia" required>
+                            <input type="text" class="form-control" id="new-title-input" name="title" placeholder="Título de la noticia" required>
                         </div>
                     </div>
+        
                     <div class="form-group row">
                         <label for="new-image-input" class="col-sm-2 col-form-label">Imagen</label>
                         <div class="col-sm-10">
-                            <input type="file" id="new-image-input" name="image-create" style="margin-top: 10px;">
+                            <input type="file" id="new-image-input" name="imagen" accept="image/*" style="margin-top: 10px;">
                         </div>
                     </div>
+        
                     <div class="form-group row">
                         <label for="new-publisher-input" class="col-sm-2 col-form-label">Autor</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="new-publisher-input" name="publisher-create" placeholder="Redactor del artículo" required>
+                            <input type="text" class="form-control" id="new-publisher-input" name="publisher" placeholder="Redactor del artículo" required>
                         </div>
                     </div>
+        
                     <div class="form-group row">
                         <label for="new-source-input" class="col-sm-2 col-form-label">Periódico</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="new-source-input" name="source-create" placeholder="Periódico de la noticia">
+                            <input type="text" class="form-control" id="new-source-input" name="source" placeholder="Periódico de la noticia">
                         </div>
                     </div>
+        
                     <div class="form-group row">
                         <label for="new-text-input" class="col-sm-2 col-form-label">Texto</label>
                         <div class="col-sm-10">
-                            <textarea rows="10" class="form-control" id="new-text-input" name="text-create" placeholder="Contenido de la noticia" required></textarea>
+                            <textarea rows="10" class="form-control" id="new-text-input" name="text" placeholder="Contenido de la noticia" required></textarea>
                         </div>
                     </div>
+        
                     <div class="form-group row">
                         <div class="col-sm-10 offset-sm-2">
                             <button type="reset" class="btn btnCrear float-right" onclick="history.back()">Cancelar</button>
-                            <button type="submit" name="button-create" class="btn btnGreenCrear float-right mr-3">Publicar</button>
+                            <button type="submit" name="button" class="btn btnGreenCrear float-right mr-3">Publicar</button>
                         </div>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </main>
 
         <?php include("footer.php");?>
